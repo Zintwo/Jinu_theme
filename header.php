f4a7ba34e9748282f5b2bdbfcf7ce1514c78562a
Zintwo <znt1401@live.co.kr>
2018-07-20 15:35:36 +0900

    헤더파일 생성

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="wrap">
+	<header id="header">
+
+	</header><!-- .site-header -->
