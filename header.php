aef1f1b203d3e221c4deedbd1f2451ec68557599
Zintwo <znt1401@live.co.kr>
2018-07-23 15:13:07 +0900

    meta theme-color
    컬러변경

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
 	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, minimum-scale=1.0">
-	<meta name="theme-color" content="#5b5b5b">
+	<meta name="theme-color" content="#23282d">
 	<?php wp_head(); ?>
 </head>
 
